5e2f7f33069b89834369ac7b6e0d5df4a5bcbbc6
John Kreitlow <jkreitlow@jwplayer.com>
2019-03-07 15:54:07 -0500

    build assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = grunt => {
         includeTitleElement: false,
         prefix: 'ds-icon-',
         svg: {
-          style: 'display:none'
+          style: 'width:0;height:0;visibility:hidden;'
         }
       },
 
