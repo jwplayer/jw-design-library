8e9953d3e00c70ebbc53a6114fbf701394d016d7
John Kreitlow <jkreitlow@jwplayer.com>
2019-03-11 16:24:56 -0400

    Add POC style-dictionary structure

---
+++ dist/js/system-colors.esm.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 11 Mar 2019 20:24:04 GMT
+ */
+
+export const DsColorSystemBlack = "#000000";
+export const DsColorSystemTransparent = "#000000";
+export const DsColorSystemBlue = "#0a75e3";
+export const DsColorSystemGreen = "#0aac29"; // affirmative
+export const DsColorSystemOrange = "#ffba43"; // medium risk
+export const DsColorSystemRed = "#ff0000"; // destructive - high risk
+export const DsColorSystemWhite = "#ffffff";
