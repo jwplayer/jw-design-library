4dcd7acad6df0fc8603ace4051005d8289c13118
John Kreitlow <jkreitlow@jwplayer.com>
2019-03-11 16:24:56 -0400

    Add POC style-dictionary structure

---
+++ dist/js/brand-colors.esm.js
@@ -0,0 +1,21 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 11 Mar 2019 20:24:04 GMT
+ */
+
+export const DsColorBrandDahliaBase = "#ec0041";
+export const DsColorBrandDahliaFade = "#fbccd9";
+export const DsColorBrandDahliaDark1 = "#bd0034";
+export const DsColorBrandDahliaDark2 = "#a5002e";
+export const DsColorBrandFogBase = "#d8dde6";
+export const DsColorBrandFogFade = "#f3f5f8";
+export const DsColorBrandFogDark1 = "#a0acc2";
+export const DsColorBrandFogDark2 = "#8797b2";
+export const DsColorBrandMidnightBase = "#475470";
+export const DsColorBrandMidnightFade = "#dadde2";
+export const DsColorBrandMidnightDark1 = "#39435a";
+export const DsColorBrandMidnightDark2 = "#323b4e";
+export const DsColorBrandAbyssBase = "#2f3542";
+export const DsColorBrandAbyssFade = "#d5d7d9";
+export const DsColorBrandAbyssDark1 = "#262a35";
+export const DsColorBrandAbyssDark2 = "#21252e";
