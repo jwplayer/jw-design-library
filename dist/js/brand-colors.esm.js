b2a5eafcb2c87149228fa2163a67f780896889a1
John Kreitlow <jkreitlow@jwplayer.com>
2019-04-03 16:30:10 -0400

    Add primary and secondary button values

--- dist/js/brand-colors.esm.js
+++ dist/js/brand-colors.esm.js
@@ -1,21 +1,25 @@
 /**
  * Do not edit directly
- * Generated on Tue, 02 Apr 2019 17:06:03 GMT
+ * Generated on Thu, 04 Apr 2019 17:08:15 GMT
  */
 
 export const dsColorBrandDahlia = "#ec0041";
 export const dsColorBrandDahliaFade = "#fbccd9";
+export const dsColorBrandDahliaAlphaFade = "rgba(236, 0, 65, 0.3)";
 export const dsColorBrandDahliaDark = "#bd0034";
 export const dsColorBrandDahliaDarker = "#a5002e";
 export const dsColorBrandFog = "#d8dde6";
 export const dsColorBrandFogFade = "#f3f5f8";
+export const dsColorBrandFogAlphaFade = "rgba(216, 221, 230, 0.3)";
 export const dsColorBrandFogDark = "#a0acc2";
 export const dsColorBrandFogDarker = "#8797b2";
 export const dsColorBrandMidnight = "#475470";
 export const dsColorBrandMidnightFade = "#dadde2";
+export const dsColorBrandMidnightAlphaFade = "rgba(71, 84, 112, 0.3)";
 export const dsColorBrandMidnightDark = "#39435a";
 export const dsColorBrandMidnightDarker = "#323b4e";
 export const dsColorBrandAbyss = "#2f3542";
 export const dsColorBrandAbyssFade = "#d5d7d9";
+export const dsColorBrandAbyssAlphaFade = "rgba(47, 53, 66, 0.3)";
 export const dsColorBrandAbyssDark = "#262a35";
 export const dsColorBrandAbyssDarker = "#21252e";
