51a46fa485b3692013f71e67ca0bd8c1623c7362
John Kreitlow <jkreitlow@jwplayer.com>
2019-04-03 16:28:19 -0400

    Stop converting everything to rems

--- scripts/transform-groups/less-transform-group.js
+++ scripts/transform-groups/less-transform-group.js
@@ -4,7 +4,6 @@ module.exports = {
 		'attribute/cti',
 		'name/cti/kebab',
 		'time/seconds',
-		'size/rem',
 		'color/css', // replaces built-in `color/hex` choice, fixes rgba
 		'content/arrayToList' // convert YAML arrays into comma-separated lists
 	]
