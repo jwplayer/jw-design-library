bb6926c28d62040ebe70334983f4696d4770a61c
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2020-05-29 11:06:39 +0200

    Add theming vars and fix missing js output

---
+++ dist/js/layout.esm.js
@@ -0,0 +1,10 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 29 May 2020 09:05:30 GMT
+ */
+
+export const dsLayoutSizeSmall = "8px";
+export const dsLayoutSizeMedium = "16px";
+export const dsLayoutSizeLarge = "24px";
+export const dsLayoutSizeExtraLarge = "32px";
+export const dsLayoutGridBase = "8px;";
