7722074eaa4fd43882c2bd177f763c869e5a7d00
Pieter Janssen <4687577+pajter@users.noreply.github.com>
2020-05-29 12:15:16 +0200

    Output components to scss, css, js

---
+++ dist/js/component/radioinput.esm.js
@@ -0,0 +1,30 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 29 May 2020 10:14:39 GMT
+ */
+
+export const dsRadioinputBorderRadius = "100%";
+export const dsRadioinputBorderStyle = "solid";
+export const dsRadioinputBorderWidth = "1px";
+export const dsRadioinputColorBackgroundUnchecked = "#ffffff";
+export const dsRadioinputColorBackgroundCheckedStatic = "#0a75e3";
+export const dsRadioinputColorBackgroundCheckedDisabled = "#d8dde6";
+export const dsRadioinputColorBorderUncheckedStatic = "#a0acc2";
+export const dsRadioinputColorBorderUncheckedHover = "#2f3542";
+export const dsRadioinputColorBorderUncheckedDisabled = "#d8dde6";
+export const dsRadioinputColorBorderCheckedStatic = "#0a75e3";
+export const dsRadioinputCursorStatic = "pointer";
+export const dsRadioinputCursorDisabled = "not-allowed";
+export const dsRadioinputSizeHeight = "16px";
+export const dsRadioinputSizeWidth = "16px";
+export const dsRadioinputFieldMarginBottom = "16px";
+export const dsRadioinputLabelColorTextDisabled = "#d8dde6";
+export const dsRadioinputLabelFontSize = "13px";
+export const dsRadioinputLabelFontLineHeight = 1.4;
+export const dsRadioinputLabelFontFamily = "\"Open Sans\", Arial, sans-serif";
+export const dsRadioinputLabelSizePaddingTop = 0;
+export const dsRadioinputLabelSizePaddingRight = "16px";
+export const dsRadioinputLabelSizePaddingBottom = 0;
+export const dsRadioinputLabelSizePaddingLeft = "8px";
+export const dsRadioinputIndicatorColor = "#ffffff";
+export const dsRadioinputIndicatorRadius = "3px";
