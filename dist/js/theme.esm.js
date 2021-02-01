bc0ac16681f86a9efc41919ed5e7b0c449499491
Monica Parra <monicaparra@MParra-MBP-13.local>
2021-02-01 14:55:35 -0500

    removing js files, updating system colors, and adding icons

--- dist/js/theme.esm.js
+++
@@ -1,9 +0,0 @@
-/**
- * Do not edit directly
- * Generated on Tue, 26 Jan 2021 19:31:45 GMT
- */
-
-export const dsThemeLightBg = "#ffffff";
-export const dsThemeLightFg = "#2f3542";
-export const dsThemeDarkBg = "#2f3542";
-export const dsThemeDarkFg = "#ffffff";
