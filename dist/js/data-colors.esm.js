794c060cda4422eec4d210ed666f1d23a90876b1
Monica Parra <monicaparra@MParra-MBP-13.local>
2021-02-17 14:28:48 -0500

    matching less with scss for data colors

--- dist/js/data-colors.esm.js
+++ dist/js/data-colors.esm.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 17 Feb 2021 19:06:29 GMT
+ * Generated on Wed, 17 Feb 2021 19:27:40 GMT
  */
 
 export const dsColorRampDataHueBlue = ["#0073b3","#2e8dc3","#51a7d2","#74c1e0","#98dbef","#bef5ff"];
