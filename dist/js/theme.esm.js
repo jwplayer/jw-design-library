e95926d6e3ce66161bb981b5aac306027b0f398e
Kim Hart <kimhart90@gmail.com>
2021-09-22 11:41:54 -0400

    added secondary text color to theme vars

--- dist/js/theme.esm.js
+++ dist/js/theme.esm.js
@@ -1,9 +1,11 @@
 /**
  * Do not edit directly
- * Generated on Tue, 10 Aug 2021 18:13:47 GMT
+ * Generated on Wed, 22 Sep 2021 15:39:52 GMT
  */
 
 export const dsThemeLightBg = "#ffffff";
 export const dsThemeLightFg = "#2f3542";
+export const dsThemeLightFgSecondary = "#475470";
 export const dsThemeDarkBg = "#21252e";
 export const dsThemeDarkFg = "#ffffff";
+export const dsThemeDarkFgSecondary = "#8797b2";
